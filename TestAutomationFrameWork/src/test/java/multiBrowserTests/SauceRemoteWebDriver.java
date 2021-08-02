0c4c1ca5f91ad7677831037c32a7beeedadea143
VisvaroobiniAgathursamy <visvaroobini.agathursamy@fourth.com>
2021-08-02 17:14:34 +0100

    Saucelabs(RemoteServer)Setup

---
+++ TestAutomationFrameWork/src/test/java/multiBrowserTests/SauceRemoteWebDriver.java
@@ -0,0 +1,56 @@
+package multiBrowserTests;
+
+import java.net.URL;
+
+import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+
+public class SauceRemoteWebDriver {
+	
+//	 public static final String USERNAME = "oauth-agathursamy.v-1306a";
+//	 public static final String ACCESS_KEY = "ab78c042-f9e4-4a21-bef8-e3958b5e1355";
+//	 public static final String URL = "http://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.eu-central-1.saucelabs.com:443/wd/hub";
+//	 
+		 public static void main(String[] args) throws Exception {
+			 
+			 
+			 
+			 MutableCapabilities sauceOptions = new MutableCapabilities();
+			 sauceOptions.setCapability("username", "oauth-agathursamy.v-1306a");
+			 sauceOptions.setCapability("accessKey", "ab78c042-f9e4-4a21-bef8-e3958b5e1355");
+			 
+		 DesiredCapabilities caps = DesiredCapabilities.chrome();
+		 caps.setCapability("sauce:options", sauceOptions);
+		 caps.setCapability("platform", "Windows 10");
+		 caps.setCapability("version", "latest");
+		 caps.setCapability("browserName", "chrome");
+		//https://oauth-agathursamy.v-1306a:ab78c042-f9e4-4a21-bef8-e3958b5e1355@ondemand.eu-central-1.saucelabs.com:443/wd/hub
+		 WebDriver driver = new RemoteWebDriver(new URL("https://ondemand.eu-central-1.saucelabs.com:443/wd/hub"), caps);
+		 driver.get("https://www.fourthhospitality.com/portal/default.asp?strCode=QC9059");
+		 driver.manage().window().maximize();
+			try {
+				Thread.sleep(10000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+		 String  title = driver.getTitle();
+		 System.out.println("PageTitle = " +title);
+			try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println("TestCompleted");
+			
+			driver.close();
+		 }
+	}
+
+
